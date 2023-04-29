0547cf3c867654d01af379b2ecd2d00539cb7b5d
spongeyperson <spongeyperson1000@live.com>
2023-04-28 19:27:29 -0700

    Update & Stage for Editing on other Machine

--- ssh-ident.sh
+++ ssh-ident.sh
@@ -3,7 +3,7 @@ echo -e "\e[0m"
 echo -e "\e[96m##################################"
 echo -e "\e[96m# Welcome to \e[93mSpongey's\e[0m           \e[96m#\e[0m"
 echo -e "\e[96m# \e[4mNo Excuses\e[0m \e[96mSSH Security Script #\e[0m"
-echo -e "\e[96m# \e[31mrev. 4\e[0m                         \e[96m#\e[0m"
+echo -e "\e[96m# \e[31mrev. 4.1\e[0m                         \e[96m#\e[0m"
 echo -e "\e[96m##################################"
 echo -e "\e[0m"
 echo -e "\e[34m::\e[37m Do you want to configure SSH identities? (y/N)\e[0m"
@@ -18,10 +18,14 @@ if [ "$choice" = "y" ]; then
     echo -e "\e[32m==> \e[0m\c\r"; read username
     echo -e "\e[34m::\e[37m Enter IP Address: \e[0m"
     echo -e "\e[32m==> \e[0m\c\r"; read ip_address
+    echo -e "\e[34m::\e[37m Enter Port: \e[0m"
+    echo -e "\e[32m==> \e[0m\c\r"; read port_number
     echo -e "\e[34m::\e[37m Enter Hostname: \e[0m"
     echo -e "\e[34m::\e[31m\e[5m \e[7mDO NOT\e[0m\e[31m append .local"
     echo -e "\e[32m==> \e[0m\c\r"; read hostname
     hostname="${hostname%.local}"
+
+## Prompt user
 
     # Create SSH identity files
     private_key=~/.ssh/ssh-identities/$hostname
@@ -34,12 +38,13 @@ if [ "$choice" = "y" ]; then
 
     # Copy identity file to remote host
     echo -e "\e[32m==\e[0m"
-    ssh-copy-id -i "$public_key" "$username@$ip_address"
+    ssh-copy-id -i "$public_key" "$username@$ip_address" -p "$port_number"
     echo -e "\e[32m==✅ \e[0mSSH Public Key Copied to \e[4m$hostname"
 
     # Append host configuration to ~/.ssh/config
     printf "Host $hostname\n\
         Hostname $ip_address\n\
+        Port $port_number\n\
         IdentityFile $private_key\n\
         User $username\n" >> ~/.ssh/config
 
